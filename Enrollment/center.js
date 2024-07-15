f673638e8f1a77f39fc5a13a9d9d91743ad6072f
Anthony Nguyen <adnguyen527@gmail.com>
2024-07-14 23:55:03 -0500

    changed to type module, created a script to filter center

--- Enrollment/center.js
+++ Enrollment/center.js
@@ -1,17 +1,19 @@
 import { filterCenter } from "../scripts/filterCenter.js";
-const {MongoClient} = require('mongodb');
+import {MongoClient} from "mongodb";
 
 async function main() {
     const user = "adnguyen527";
     const pass = "ku7IxM1AuiwqrV9e";
     const uri = `mongodb+srv://${user}:${pass}@studentdirectory.eil6uvt.mongodb.net/`;
-    const center = process.argv[2];
+    var center = ((process.argv[2]) ? process.argv[2] : "Southlake");
     const client = new MongoClient(uri);
     try {
         await client.connect();
         const coll = client.db("StudentDirectory").collection("enrollment_reports");
-        console.log(await filterCenter(coll, center));
-        console.log("done");
+        const cursor = filterCenter(coll, center);
+        for await (const doc of cursor) {
+            console.dir(doc);
+        }
     } catch (e) {
         console.error(e);
     } finally {
